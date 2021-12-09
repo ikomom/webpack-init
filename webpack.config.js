123f64b587b920a03357341a163e1412f523e33c
yzm <315793076@qq.com>
2021-12-09 18:09:57 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,18 @@
 const path = require('path')
 
 module.exports = {
-    entry: path.join(__dirname, './src/index.js'),
+    entry: path.join(__dirname, './src/index.css'),
     output:{
         // 把所有依赖的模块合并输出到一个 bundle.js 文件
         filename: 'bundle.js', //打包后的文件名称
         path: path.resolve(__dirname,'./dist') //打包后的目录
+    },
+    module: {
+        rules: [ // 转换规则
+            {
+                test: /\.css$/, //匹配所有的 css 文件
+                use: 'css-loader' // use: 对应的 Loader 名称
+            }
+        ]
     }
 }
