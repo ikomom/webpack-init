c7b68e31a9f640ddef68054f19179024e94fddec
ikonon <315793076@qq.com>
2021-12-11 23:26:21 +0800

    lazy style

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import a from './index.css'
+
+console.log(a);
+
+// import './index.css'
 import b, {page} from "./Page";
 
+import lazy from './index.lazy.css'
+
+console.log(lazy)
 const html = `
   <component>
     <title>Super Title</title>
     <text>Awesome Text</text>
   </component>
 `;
-
+let click = false
+window.changeTheme = (e) => {
+  if(!click) {
+    lazy.use()
+    click = true
+  } else {
+    lazy.unuse()
+    click = false
+  }
+}
 console.log(html);
-console.log(a);
 console.log(page, b);
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,48 @@
 const path = require('path')
 
 module.exports = {
-    devtool: 'source-map',
-    entry: path.join(__dirname, './src/index.js'),
-    output:{
-        // 把所有依赖的模块合并输出到一个 bundle.js 文件
-        filename: 'bundle.js', //打包后的文件名称
-        path: path.resolve(__dirname,'./dist') //打包后的目录
-    },
-    module: {
-        rules: [ // 转换规则
-            {
-                test: /\.css$/, //匹配所有的 css 文件
-                use: 'css-loader' // use: 对应的 Loader 名称
+  devtool: 'source-map',
+  entry: path.join(__dirname, './src/index.js'),
+  output: {
+    // 把所有依赖的模块合并输出到一个 bundle.js 文件
+    filename: 'bundle.js', //打包后的文件名称
+    path: path.resolve(__dirname, './dist') //打包后的目录
+  },
+  module: {
+    rules: [ // 转换规则
+      {
+        test: /\.lazy\.css$/i,
+        use: [
+          { loader: "style-loader", options: { injectType: "lazyStyleTag" } },
+          {loader: "css-loader", options: {modules: true}}
+        ],
+      },
+      {
+        test: /\.css$/i, //匹配所有的 css 文件
+        exclude: /\.lazy\.css$/i,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              insert: "body",
+              // injectType: 'singletonStyleTag'
             }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              exportType: 'array',
+              esModule: true,
+              modules: {// 开启css-modules模式, 默认值为flase
+                localIdentName: '[path][name]__[local]--[hash:base64:5]', //设置css-modules模式下local类名的命名
+                // exportLocalsConvention : "camelCase" ,
+                // exportGlobals: true,
+              },
+
+            }
+          }
         ]
-    }
+      },
+    ]
+  }
 }
