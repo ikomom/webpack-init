e7bbba933aa11531fd39ccc9c8ad125fd58198ba
ikonon <315793076@qq.com>
2021-12-20 21:38:06 +0800

    asset

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,10 @@
 import {log} from "../../utils/utils";
+import Sketchpad from '../../assets/images/Sketchpad.png'
+import Kani from '../../assets/images/kani.svg'
+import hellowMd from '../../assets/file/hellow.md'
+import hellowTxt from '../../assets/file/hellow.txt'
+import testJson from '../../assets/json/test.json'
 
 export const name = 'index'
 
-log({name})
+log({name, Sketchpad, Kani, hellowMd, hellowTxt, testJson})
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ConsoleLogWebpackPlugin = require('./plugin/ConsoleLog')
+const svgToMiniDataURI = require('mini-svg-data-uri');
 
 const getPages = (names, prefix = './src/pages/') => {
   const entry = {}
@@ -18,6 +19,7 @@ module.exports = [
     entry: getPages(['index', 'viewport']),
     output: {
       path: path.resolve(__dirname, './multi-page'),
+      // assetModuleFilename: 'images/[name][ext][query]'
     },
     plugins: [
       new ConsoleLogWebpackPlugin(),
@@ -26,13 +28,38 @@ module.exports = [
         // chunks:['viewport']
       })
     ],
+    module: {
+      rules: [
+        {
+          test: /\.png/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'static/[name][hash][ext][query]'
+          }
+        },
+        {
+          test: /\.svg/,
+          type: 'asset/inline',
+          generator: {
+            dataUrl: content => {
+              content = content.toString();
+              return svgToMiniDataURI(content);
+            }
+          }
+        },
+        {
+          test: /(\.txt|\.md)/,
+          type: 'asset/source',
+        }
+      ]
+    }
   },
-  {
-    name: 'test2',
-    mode: 'production',
-    entry: getPages(['index']),
-    output: {
-      path: path.resolve(__dirname, './multi-page-2'),
-    },
-  }
+  // {
+  //   name: 'test2',
+  //   mode: 'production',
+  //   entry: getPages(['index']),
+  //   output: {
+  //     path: path.resolve(__dirname, './multi-page-2'),
+  //   },
+  // }
 ]
